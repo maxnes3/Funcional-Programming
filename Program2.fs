3d10bafc53e30d40be7e3ec321e582412568189f
maxnes3 <draw_max@mail.ru>
2024-09-23 23:34:39 +0400

    Лабораторная работа №2

---
+++ Program2.fs
@@ -0,0 +1,22 @@
+﻿(*Назавание модуля*)
+module Program2
+
+(*Импорт модулей*)
+open System
+open System.IO
+
+// Функция для подсчета гласных в тексте
+let countVowels (text: string) =
+    let vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ"
+    text
+    |> Seq.filter (fun c -> vowels.Contains(c))
+    |> Seq.groupBy id
+    |> Seq.map (fun (vowel, occurrences) -> vowel, Seq.length occurrences)
+    |> Map.ofSeq
+
+// Чтение текста из файла
+let readTextFromFile (filePath: string) =
+    if File.Exists(filePath) then
+        File.ReadAllText(filePath)
+    else
+        failwithf "File not found: %s" filePath
---
+++ main.fs
@@ -0,0 +1,29 @@
+﻿(*Назавание модуля*)
+module main
+
+(*Импорт модулей*)
+open Program
+open Program2
+
+(*Начальная точка входа программы*)
+[<EntryPoint>]
+let main argv = 
+    (* Лаборатоная работа #1 *)
+    (*printfn "Сумма чисел от 1 до 100, содержащих цифру 9: %d" sum
+    0*)
+
+    (* Лабораторная работа #2 *)
+    if argv.Length <> 1 then
+        printfn "Usage: <program> <file-path>"
+        1
+    else
+        let filePath = argv.[0]
+        let text = readTextFromFile filePath
+        let vowelCounts = countVowels text
+
+        printfn "Vowel counts:"
+        vowelCounts |> Map.iter (fun vowel count -> printfn "%c: %d" vowel count)
+
+        0
+
+    (* Лабораторная работа #3 *)
