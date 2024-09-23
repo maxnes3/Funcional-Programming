26a3a187403c9846936e321260a01f699a3e0993
maxnes3 <draw_max@mail.ru>
2024-09-23 18:38:15 +0400

    Первая лабораторная работа готова!

--- Program.fs
+++ Program.fs
@@ -1,2 +1,18 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿(*Функция конвертирует число в строчку и проверяет наличие символа 9*)
+let containsNine n =
+    n.ToString().Contains('9')
+
+(*Создание массива чисел от 1 до 100*)
+let numbers = [1..100]
+
+(*Функция проверяет значение по состветствию условия containsNine и находит их сумму*)
+let sum = 
+    numbers 
+    |> List.filter containsNine
+    |> List.sum
+
+(*Начальная точка входа программы*)
+[<EntryPoint>]
+let main _ = 
+    printfn "Сумма чисел от 1 до 100, содержащих цифру 9: %d" sum
+    0
